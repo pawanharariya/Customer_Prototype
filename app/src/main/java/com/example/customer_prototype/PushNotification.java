44b7cfda140394715ea1dec55a39e3c84a1981a9
Sagar <sagarjain268380@gmail.com>
2020-03-06 15:45:31 +0530

    testing java

--- app/src/main/java/com/example/customer_prototype/PushNotification.java
+++ app/src/main/java/com/example/customer_prototype/PushNotification.java
@@ -27,7 +27,9 @@ public class PushNotification extends FirebaseMessagingService {
 
         Intent intent=new Intent(this,MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
         PendingIntent pendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+
         Uri soundURI= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
         NotificationCompat.Builder notifivationbuilder=new NotificationCompat.Builder(this)
