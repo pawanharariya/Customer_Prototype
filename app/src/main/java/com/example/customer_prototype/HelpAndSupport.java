a38b950f2bb92e3340186ef8a82157896d8470db
Sagar <sagarjain268380@gmail.com>
2020-03-08 14:22:39 +0530

    Merge branch 'master' of C:\Users\sagar jain\Pictures\Customer_Prototype\Customer_Prototype with conflicts.

--- app/src/main/java/com/example/customer_prototype/HelpAndSupport.java
+++ app/src/main/java/com/example/customer_prototype/HelpAndSupport.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class HelpAndSupport extends AppCompatActivity {
 
     Button btnHistory,btnRequest;
+    TextView txtName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/customer_prototype/MyAccoutn.java
+++ app/src/main/java/com/example/customer_prototype/MyAccoutn.java
@@ -73,7 +73,12 @@ public class MyAccoutn extends AppCompatActivity {
 
 
 
-
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
        updateProfilePic.setOnClickListener(new View.OnClickListener() {
             @Override
