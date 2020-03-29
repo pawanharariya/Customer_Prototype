2945511c6350e76c743a5811ce1b70ae980d8dbf
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-03-29 17:18:02 +0530

    Book Parking added

---
+++ app/src/main/java/com/example/customer_prototype/Parking_DataSetFirebase.java
@@ -0,0 +1,9 @@
+package com.example.customer_prototype;
+
+public class Parking_DataSetFirebase {
+
+    public String status,orderid,date,shop,profile;
+
+    public Parking_DataSetFirebase() {
+    }
+}
