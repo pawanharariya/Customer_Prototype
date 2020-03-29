2772e4e20f5768252862a70a6a9ffaa97983a5e7
Tarun Kumar Jain <tarunjain1st@gmail.com>
2020-03-29 19:19:14 +0530

    Refer Completed

--- app/src/main/java/com/example/customer_prototype/Refer.java
+++ app/src/main/java/com/example/customer_prototype/Refer.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
@@ -19,8 +20,12 @@ public class Refer extends AppCompatActivity {
         setContentView(R.layout.activity_refer);
         ActionBar actionBar=getSupportActionBar();
         actionBar.setTitle("Refer");
+
         Button know_more = findViewById(R.id.knowmore_btn);
+        LinearLayout linearLayout = findViewById(R.id.linearLayout);
         know_more.setOnClickListener(new Click());
+        linearLayout.setOnClickListener(new Click());
+
     }
 
     public class Click implements View.OnClickListener{
@@ -32,9 +37,21 @@ public class Refer extends AppCompatActivity {
                     Intent intent = new Intent(Refer.this,Refer_Info.class);
                     startActivity(intent);
                     break;
+                case R.id.linearLayout:
+                    share();
+                    break;
+
                 default:
 
             }
         }
+    }
+    public void share()
+    {
+        Intent share_intent = new Intent();
+        share_intent.setAction(Intent.ACTION_SEND);
+        share_intent.putExtra(Intent.EXTRA_TEXT,"http://www.google.com");
+        share_intent.setType("text/plain");
+        startActivity(Intent.createChooser(share_intent,"share via"));
     }
 }
